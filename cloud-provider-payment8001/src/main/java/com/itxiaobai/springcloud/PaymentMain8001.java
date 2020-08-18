82c7759bb2f12e893f9c247117ff592294011fdf
Your Name <lsen1124@163.com>
2020-08-18 09:39:50 +0800

    service  文件提交

--- cloud-provider-payment8001/src/main/java/com/itxiaobai/springcloud/PaymentMain8001.java
+++ cloud-provider-payment8001/src/main/java/com/itxiaobai/springcloud/PaymentMain8001.java
@@ -1,10 +1,14 @@
 package com.itxiaobai.springcloud;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableDiscoveryClient
+@EnableCircuitBreaker
 @SpringBootApplication
+@MapperScan("com.itxiaobai.mapper")
 public class PaymentMain8001
 {
     public static void main(String[] args) {
