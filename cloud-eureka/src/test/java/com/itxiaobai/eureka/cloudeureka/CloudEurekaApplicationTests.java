9b70608bdc993eade995c4c9a4e5b7854cd3d782
lisen <15555797523@189.cn>
2021-01-23 21:42:59 +0800

    配置Eureka

---
+++ cloud-eureka/src/test/java/com/itxiaobai/eureka/cloudeureka/CloudEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itxiaobai.eureka.cloudeureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
